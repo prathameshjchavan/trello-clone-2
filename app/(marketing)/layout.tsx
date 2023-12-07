76af0d19975c284365f7d8760ae1ae09bb4e0e09
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-07 11:54:13 +0530

    added navbar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Navbar from "./_components/navbar";
 
 interface MarketingLayoutProps {
   children: ReactNode;
@@ -7,6 +8,7 @@ interface MarketingLayoutProps {
 const MarketingLayout = ({ children }: MarketingLayoutProps) => {
   return (
     <div className="h-full bg-slate-100">
+      <Navbar />
       <main className="pb-20 pt-40">{children}</main>
     </div>
   );
