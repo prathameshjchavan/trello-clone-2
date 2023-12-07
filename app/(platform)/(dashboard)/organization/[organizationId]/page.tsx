a71f74809bb4f965e7c9e246191fc4a92a2593e0
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-07 15:24:55 +0530

    added organization page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,11 @@
+interface OrganizationPageProps {
+  params: {
+    organizationId: string;
+  };
+}
+
+const OrganizationPage = ({ params }: OrganizationPageProps) => {
+  return <div>Organization Page</div>;
+};
+
+export default OrganizationPage;
