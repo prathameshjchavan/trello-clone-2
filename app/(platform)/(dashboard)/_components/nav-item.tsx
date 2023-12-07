065483a5076d6994ccc7e3505aaf68dbcd060520
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-07 22:27:38 +0530

    added accordian for nav item

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,8 +1,16 @@
 "use client";
 
-import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import { cn } from "@/lib/utils";
+import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import Image from "next/image";
+import { useMemo } from "react";
+import { useRouter, usePathname } from "next/navigation";
+import { Button } from "@/components/ui/button";
 
 export type Organization = {
   id: string;
@@ -24,6 +32,39 @@ const NavItem = ({
   organization,
   onExpand,
 }: NavItemProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const routes = useMemo(
+    () => [
+      {
+        label: "Boards",
+        icon: <Layout className="mr-2 h-4 w-4" />,
+        href: `/organization/${organization.id}`,
+      },
+      {
+        label: "Activity",
+        icon: <Activity className="mr-2 h-4 w-4" />,
+        href: `/organization/${organization.id}/activity`,
+      },
+      {
+        label: "Settings",
+        icon: <Settings className="mr-2 h-4 w-4" />,
+        href: `/organization/${organization.id}/settings`,
+      },
+      {
+        label: "Billing",
+        icon: <CreditCard className="mr-2 h-4 w-4" />,
+        href: `/organization/${organization.id}/billing`,
+      },
+    ],
+    [organization.id],
+  );
+
+  const onClick = (href: string) => {
+    router.push(href);
+  };
+
   return (
     <AccordionItem value={organization.id} className="border-none">
       <AccordionTrigger
@@ -45,6 +86,23 @@ const NavItem = ({
           <span className="text-sm font-medium">{organization.name}</span>
         </div>
       </AccordionTrigger>
+      <AccordionContent className="pt-1 text-neutral-700">
+        {routes.map((route) => (
+          <Button
+            key={route.href}
+            size="sm"
+            onClick={() => onClick(route.href)}
+            className={cn(
+              "mb-1 w-full justify-start pl-10 font-normal",
+              pathname === route.href && "bg-sky-500/10 text-sky-700",
+            )}
+            variant="ghost"
+          >
+            {route.icon}
+            {route.label}
+          </Button>
+        ))}
+      </AccordionContent>
     </AccordionItem>
   );
 };
