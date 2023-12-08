ad3f2201fd0fdad13524ec6c2bdf69fb71c18a3d
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-08 18:45:38 +0530

    creating boards functionality

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,36 @@
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+
 interface OrganizationPageProps {
   params: {
     organizationId: string;
   };
 }
 
-const OrganizationPage = ({ params }: OrganizationPageProps) => {
-  return <div>Organization Page</div>;
+const OrganizationPage = async ({ params }: OrganizationPageProps) => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <form action={create}>
+        <input
+          id="title"
+          name="title"
+          type="text"
+          placeholder="Enter a board title"
+          className="border border-black p-1"
+          required
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <div key={board.id}>{board.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationPage;
