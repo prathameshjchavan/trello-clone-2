9fb2d2e6634d4d514d328df16fbe25b094fb1da3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-09 14:31:36 +0530

    remove testing files

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,3 @@
-import { db } from "@/lib/db";
-import Board from "./_components/board";
-import Form from "./_components/form";
-
 interface OrganizationPageProps {
   params: {
     organizationId: string;
@@ -9,18 +5,7 @@ interface OrganizationPageProps {
 }
 
 const OrganizationPage = async ({ params }: OrganizationPageProps) => {
-  const boards = await db.board.findMany();
-
-  return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} board={board} />
-        ))}
-      </div>
-    </div>
-  );
+  return <div className="flex flex-col space-y-4"></div>;
 };
 
 export default OrganizationPage;
