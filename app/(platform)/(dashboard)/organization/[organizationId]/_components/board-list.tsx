2e2b4afa1f08145184cc226778c7aba8871bdde7
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-09 18:54:03 +0530

    added form popover

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,3 +1,4 @@
+import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react";
 import React from "react";
@@ -10,19 +11,21 @@ const BoardList = () => {
         Your boards
       </div>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-        <div
-          role="button"
-          className="relative flex aspect-video h-full w-full flex-col items-center justify-center space-y-1 rounded-sm bg-muted transition hover:opacity-75"
-        >
-          <p className="text-sm">Create new board</p>
-          <span className="text-xs">5 remaining</span>
-          <Hint
-            sideOffset={40}
-            description="Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace."
+        <FormPopover sideOffset={10} side="right">
+          <div
+            role="button"
+            className="relative flex aspect-video h-full w-full flex-col items-center justify-center space-y-1 rounded-sm bg-muted transition hover:opacity-75"
           >
-            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
-          </Hint>
-        </div>
+            <p className="text-sm">Create new board</p>
+            <span className="text-xs">5 remaining</span>
+            <Hint
+              sideOffset={40}
+              description="Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace."
+            >
+              <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]" />
+            </Hint>
+          </div>
+        </FormPopover>
       </div>
     </div>
   );
---
+++ components/form/form-popover.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { X } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+  Popover,
+  PopoverClose,
+  PopoverContent,
+  PopoverTrigger,
+} from "../ui/popover";
+import FormInput from "./form-input";
+import FormSubmit from "./form-submit";
+import { useAction } from "@/hooks/use-action";
+import { createBoard } from "@/actions/create-board";
+
+interface FormPopoverProps {
+  children: React.ReactNode;
+  side?: "left" | "right" | "top" | "bottom";
+  align?: "start" | "center" | "end";
+  sideOffset?: number;
+}
+
+const FormPopover = ({
+  children,
+  side = "bottom",
+  align,
+  sideOffset = 0,
+}: FormPopoverProps) => {
+  const { execute, fieldErrors } = useAction(createBoard, {
+    onSuccess: (data) => {
+      console.log({ data });
+    },
+    onError: (error) => {
+      console.log({ error });
+    },
+  });
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+
+    execute({ title });
+  };
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>{children}</PopoverTrigger>
+      <PopoverContent
+        align={align}
+        className="w-80 pt-3"
+        side={side}
+        sideOffset={sideOffset}
+      >
+        <div className="pb-4 text-center text-sm font-medium text-neutral-600">
+          Create board
+        </div>
+        <PopoverClose asChild>
+          <Button
+            variant="ghost"
+            className="absolute right-2 top-2 h-auto w-auto p-2"
+          >
+            <X className="h-4 w-4" />
+          </Button>
+        </PopoverClose>
+        <form action={onSubmit} className="space-y-4">
+          <div className="space-y-4">
+            <FormInput
+              id="title"
+              label="Board title"
+              type="text"
+              errors={fieldErrors}
+            />
+          </div>
+          <FormSubmit className="w-full">Create</FormSubmit>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
+};
+
+export default FormPopover;
