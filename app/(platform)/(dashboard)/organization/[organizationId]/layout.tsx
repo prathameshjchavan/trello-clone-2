8ea03458ff8779b2e013d1f856897fd023023c8e
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-10 20:33:31 +0530

    added title for organization

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,8 +1,18 @@
 import { Fragment, ReactNode } from "react";
 import OrgControl from "./_components/org-control";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs";
 
 interface OrganizationIdLayoutProps {
   children: ReactNode;
+}
+
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
 }
 
 const OrganizationIdLayout = ({ children }: OrganizationIdLayoutProps) => {
