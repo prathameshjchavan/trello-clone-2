a1bafffac2cb72cbecfd274fedc404d0d37a3c1b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-10 20:49:57 +0530

    added board id page

---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,11 @@
+interface BoardIdProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = ({ params }: BoardIdProps) => {
+  return <div className="">BoardIdPage</div>;
+};
+
+export default BoardIdPage;
