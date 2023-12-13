6df1c470816dcfd7ff4cfd92fe502a07a5e6346a
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2023-12-13 11:32:45 +0530

    added list input

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -1,9 +1,66 @@
 "use client";
 
+import { Plus } from "lucide-react";
+import ListWrapper from "./list-wrapper";
+import { ElementRef, useRef, useState } from "react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import FormInput from "@/components/form/form-input";
+
 interface ListFormProps {}
 
 const ListForm = (props: ListFormProps) => {
-  return <div>ListForm</div>;
+  const formRef = useRef<ElementRef<"form">>(null);
+  const inputRef = useRef<ElementRef<"input">>(null);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef, disableEditing);
+
+  if (isEditing)
+    return (
+      <ListWrapper>
+        <form
+          ref={formRef}
+          className="w-full space-y-4 rounded-md bg-white p-3 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            id="title"
+            placeholder="Enter list title..."
+            className="h-7 border-transparent px-2 py-1 text-sm font-medium transition hover:border-input focus:border-input"
+          />
+        </form>
+      </ListWrapper>
+    );
+
+  return (
+    <ListWrapper>
+      <button
+        onClick={enableEditing}
+        className="flex w-full items-center rounded-md bg-white/80 p-3 text-sm font-medium transition hover:bg-white/50"
+      >
+        <Plus className="mr-2 h-4 w-4" />
+        Add a list
+      </button>
+    </ListWrapper>
+  );
 };
 
 export default ListForm;
