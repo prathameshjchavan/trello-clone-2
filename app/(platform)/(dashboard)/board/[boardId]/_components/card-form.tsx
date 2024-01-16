701c78602aec109b3bbfa49afef2db7425144bf6
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-16 21:03:46 +0530

    implemented add card title ui

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import FormSubmit from "@/components/form/form-submit";
+import FormTextarea from "@/components/form/form-textarea";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import { forwardRef } from "react";
 
 interface CardFormProps {
@@ -13,6 +15,26 @@ interface CardFormProps {
 
 const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(
   ({ listId, isEditing, enableEditing, disableEditing }, ref) => {
+    if (isEditing) {
+      return (
+        <form className="m-1 space-y-4 px-1 py-0.5">
+          <FormTextarea
+            id="title"
+            onKeyDown={() => {}}
+            ref={ref}
+            placeholder="Enter a title for this card..."
+          />
+          <input hidden id="listId" name="listId" value={listId} />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add card</FormSubmit>
+            <Button onClick={disableEditing} size="sm" variant="ghost">
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      );
+    }
+
     return (
       <div className="px-2 pt-2">
         <Button
