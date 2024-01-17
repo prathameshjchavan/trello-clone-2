fcde535337070133620c7427ee352b3f924cd0c6
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-17 20:07:54 +0530

    displaying list items

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Card } from "@prisma/client";
+
+interface CardItemProps {
+  data: Card;
+  index: number;
+}
+
+const CardItem = ({ data, index }: CardItemProps) => {
+  return (
+    <div className="truncate rounded-md border-2 border-transparent bg-white px-3 py-2 text-sm shadow-sm hover:border-black">
+      {data.title}
+    </div>
+  );
+};
+
+export default CardItem;
--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -4,6 +4,8 @@ import { ListWithCards } from "@/typings";
 import React, { ElementRef, useRef, useState } from "react";
 import ListHeader from "./list-header";
 import CardForm from "./card-form";
+import { cn } from "@/lib/utils";
+import CardItem from "./card-item";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -30,13 +32,23 @@ const ListItem = ({ data, index }: ListItemProps) => {
     <div className="h-full w-[272px] shrink-0 select-none">
       <div className="w-full rounded-md bg-[#F1F2F4] pb-2 shadow-md">
         <ListHeader onAddCard={enableEditing} data={data} />
+        <ol
+          className={cn(
+            "mx-1 flex flex-col gap-y-2 px-1 py-0.5",
+            data.cards.length > 0 ? "mt-2" : "mt-0",
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem index={index} key={card.id} data={card} />
+          ))}
+        </ol>
         <CardForm
-        listId={data.id}
-        ref={textareaRef}
-        isEditing={isEditing}
-        enableEditing={enableEditing}
-        disableEditing={disableEditing}
-      />
+          listId={data.id}
+          ref={textareaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
+        />
       </div>
     </div>
   );
