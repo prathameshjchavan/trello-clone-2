009f1b52dc81cb7bf3309731e973060364060ac8
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-19 14:06:45 +0530

    added list in card dialog

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { ElementRef, useRef, useState } from "react";
 import { Layout } from "lucide-react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useParams } from "next/navigation";
 
 import FormInput from "@/components/form/form-input";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -13,18 +15,36 @@ interface HeaderProps {
 
 const Header = ({ data }: HeaderProps) => {
   const [title, setTitle] = useState(data.title);
+  const queryClient = useQueryClient();
+  const params = useParams();
+  const inputRef = useRef<ElementRef<"input">>(null);
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title");
+
+    console.log({ title });
+  };
 
   return (
     <div className="mb-6 flex w-full items-start gap-x-3">
       <Layout className="mt-1 h-5 w-5 text-neutral-700" />
       <div className="w-full">
-        <form>
+        <form action={onSubmit}>
           <FormInput
             id="title"
+            ref={inputRef}
+            onBlur={onBlur}
             defaultValue={title}
             className="relative -left-1.5 mb-0.5 w-[95%] truncate border-transparent bg-transparent px-1 text-xl font-semibold text-neutral-700 focus-visible:border-input focus-visible:bg-white"
           />
         </form>
+        <p className="text-sm text-muted-foreground">
+          in list <span className="underline">{data.list.title}</span>
+        </p>
       </div>
     </div>
   );
