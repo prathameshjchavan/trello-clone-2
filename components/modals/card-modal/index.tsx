849c0861390a25a0da41a16edbf98f6f4c780660
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-19 13:06:26 +0530

    added dialog

---
+++ components/modals/card-modal/index.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { useCardModal } from "@/hooks/use-card-modal"
+
+type Props = {}
+
+const CardModal = (props: Props) => {
+    const {id, isOpen, onClose} = useCardModal()
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+        <DialogContent>
+
+        </DialogContent>
+    </Dialog>
+  )
+}
+
+export default CardModal
