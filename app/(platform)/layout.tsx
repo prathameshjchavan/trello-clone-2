ce7ca3ad24503679b50f73ae21ae878a5636cba5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-19 13:27:37 +0530

    added query provider

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -3,6 +3,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 import ModalProvider from "@/components/providers/modal-provider";
+import QueryProvider from "@/components/providers/query-provider";
 
 interface PlatformLayoutProps {
   children: ReactNode;
@@ -12,9 +13,11 @@ const PlatformLayout = ({ children }: PlatformLayoutProps) => {
   return (
     <div className="h-full">
       <ClerkProvider>
-        <Toaster />
-        <ModalProvider />
-        {children}
+        <QueryProvider>
+          <Toaster />
+          <ModalProvider />
+          {children}
+        </QueryProvider>
       </ClerkProvider>
     </div>
   );
