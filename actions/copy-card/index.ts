aa4dbdaed87c9a9338f75721bd96e118a12bd99b
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 14:10:44 +0530

    added audit log for copy card

--- actions/copy-card/index.ts
+++ actions/copy-card/index.ts
@@ -1,11 +1,14 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CopyCard } from "./schema";
+import createAuditLog from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -51,6 +54,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         listId: cardToCopy.listId,
       },
     });
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE
+    })
   } catch (error) {
     return {
       error: "Failed to copy.",
