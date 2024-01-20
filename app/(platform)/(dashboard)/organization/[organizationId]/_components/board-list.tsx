ce82b11187e479db8ad88311914e196537e0a7d5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 19:30:16 +0530

    displaying available count

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,11 +1,14 @@
+import { auth } from "@clerk/nextjs";
+import Link from "next/link";
+import { HelpCircle, User2 } from "lucide-react";
+import { redirect } from "next/navigation";
+
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/hint";
 import { Skeleton } from "@/components/ui/skeleton";
 import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
-import { HelpCircle, User2 } from "lucide-react";
-import Link from "next/link";
-import { redirect } from "next/navigation";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
+import { getAvailableCount } from "@/lib/org-limit";
 
 const BoardList = async () => {
   const { orgId } = auth();
@@ -20,6 +23,8 @@ const BoardList = async () => {
       createdAt: "desc",
     },
   });
+
+  const availableCount = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -46,7 +51,9 @@ const BoardList = async () => {
             className="relative flex aspect-video h-full w-full flex-col items-center justify-center space-y-1 rounded-sm bg-muted transition hover:opacity-75"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${
+              MAX_FREE_BOARDS - availableCount
+            } remaining`}</span>
             <Hint
               sideOffset={40}
               description="Free workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace."
