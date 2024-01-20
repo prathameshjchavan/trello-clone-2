790bad25f4857069e49b3067c1aad60413dd4507
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 14:08:47 +0530

    displaying activity logs in card modal

--- components/modals/card-modal/activity.tsx
+++ components/modals/card-modal/activity.tsx
@@ -4,23 +4,26 @@ import { ActivityIcon } from "lucide-react";
 import { AuditLog } from "@prisma/client";
 
 import { Skeleton } from "@/components/ui/skeleton";
+import ActivityItem from "@/components/activity-item";
 
 interface ActivityProps {
   items: AuditLog[];
 }
 
 const Activity = ({ items }: ActivityProps) => {
-  return <div className="flex items-start gap-x-3 w-full">
-    <ActivityIcon className="h-5 w-5 mt-0.5 text-neutral-700" />
-    <div className="w-full">
-        <p className="font-semibold text-neutral-700 mb-2">Activity</p>
+  return (
+    <div className="flex w-full items-start gap-x-3">
+      <ActivityIcon className="mt-0.5 h-5 w-5 text-neutral-700" />
+      <div className="w-full">
+        <p className="mb-2 font-semibold text-neutral-700">Activity</p>
         <ol className="mt-2 space-y-4">
-            {items.map(item => (
-                <p key={item.id}>{item.entityTitle}</p>
-            ))}
+          {items.map((item) => (
+            <ActivityItem key={item.id} data={item} />
+          ))}
         </ol>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 Activity.Skeleton = function ActivitySkeleton() {
