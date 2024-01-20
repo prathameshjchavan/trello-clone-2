c8f767f6a01bcecfd35ab2268c4213b5775d62e6
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 12:36:47 +0530

    added audit log for create card

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -1,11 +1,14 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { revalidatePath } from "next/cache";
+
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateCard } from "./schema";
+import createAuditLog from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -50,6 +53,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         order: newOrder,
       },
     });
+
+    await createAuditLog({ 
+      entityId: card.id,
+      entityTitle: card.title,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.CREATE,
+     })
   } catch (error) {
     return {
       error: "Failed to create.",
