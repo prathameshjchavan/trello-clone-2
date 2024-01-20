0ad02eb6648bbdc7671240aa7f9dcba5a8aa1311
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 11:03:35 +0530

    implemented card copy and delete functionality

--- components/modals/card-modal/actions.tsx
+++ components/modals/card-modal/actions.tsx
@@ -1,25 +1,88 @@
 "use client";
 
 import { Copy, Trash } from "lucide-react";
+import { useParams } from "next/navigation";
+import { toast } from "sonner";
 
 import { Button } from "@/components/ui/button";
 import { Skeleton } from "@/components/ui/skeleton";
 import { CardWithList } from "@/typings";
+import { useAction } from "@/hooks/use-action";
+import { copyCard } from "@/actions/copy-card";
+import { deleteCard } from "@/actions/delete-card";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface ActionsProps {
   data: CardWithList;
 }
 
 const Actions = ({ data }: ActionsProps) => {
+  const params = useParams();
+  const cardModal = useCardModal();
+  const { execute: executeCopyCard, isLoading: isLoadingCopy } = useAction(
+    copyCard,
+    {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title}" copied`);
+        cardModal.onClose();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    },
+  );
+  const { execute: executeDeleteCard, isLoading: isLoadingDelete } = useAction(
+    deleteCard,
+    {
+      onSuccess: (data) => {
+        toast.success(`Card "${data.title}" deleted`);
+        cardModal.onClose();
+      },
+      onError: (error) => {
+        toast.error(error);
+      },
+    },
+  );
+
+  const onCopy = () => {
+    const boardId = params.boardId as string;
+
+    executeCopyCard({
+      id: data.id,
+      boardId,
+    });
+  };
+
+  const onDelete = () => {
+    const boardId = params.boardId as string;
+
+    executeDeleteCard({
+      id: data.id,
+      boardId,
+    });
+  };
+
   return (
     <div className="mt-2 space-y-2">
       <p className="text-xs font-semibold">Actions</p>
-      <Button variant="gray" className="w-full justify-start" size="inline">
-        <Copy className="h-4 w-4 mr-2" />
+      <Button
+        onClick={onCopy}
+        disabled={isLoadingCopy}
+        variant="gray"
+        className="w-full justify-start"
+        size="inline"
+      >
+        <Copy className="mr-2 h-4 w-4" />
         Copy
       </Button>
-      <Button variant="gray" className="w-full justify-start" size="inline">
-        <Trash className="h-4 w-4 mr-2" />
+      <Button
+        onClick={onDelete}
+        disabled={isLoadingDelete}
+        variant="gray"
+        className="w-full justify-start"
+        size="inline"
+      >
+        <Trash className="mr-2 h-4 w-4" />
         Delete
       </Button>
     </div>
