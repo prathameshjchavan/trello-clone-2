848a8fb6e3321c444c3aaf44df627c8522677fc5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 14:19:16 +0530

    added audit log for delete list

--- actions/delete-list/index.ts
+++ actions/delete-list/index.ts
@@ -1,11 +1,14 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { DeleteList } from "./schema";
+import createAuditLog from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -27,6 +30,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         board: { orgId },
       },
     });
+
+    await createAuditLog({
+      entityTitle: list.title,
+      entityId: list.id,
+      entityType: ENTITY_TYPE.LIST,
+      action: ACTION.DELETE
+    })
   } catch (error) {
     return {
       error: "Failed to delete.",
