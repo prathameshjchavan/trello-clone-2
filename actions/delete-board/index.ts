af8edd59af189e624be0db643d411bce035e1337
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 14:17:14 +0530

    added audit log for delete board

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -1,14 +1,15 @@
 "use server";
 
 import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
-import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { DeleteBoard } from "./schema";
-import { redirect } from "next/navigation";
 import createAuditLog from "@/lib/create-audit-log";
-import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
