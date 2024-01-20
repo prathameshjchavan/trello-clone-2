506bf4b5b0b9ca25fdf217977f88afde15453d47
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-01-20 14:13:43 +0530

    added audit log for create board

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { auth } from "@clerk/nextjs";
+import { revalidatePath } from "next/cache";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+
 import { createSafeAction } from "@/lib/create-safe-action";
 import { db } from "./../../lib/db";
-import { auth } from "@clerk/nextjs";
 import { InputType, ReturnType } from "./types";
-import { revalidatePath } from "next/cache";
 import { CreateBoard } from "./schema";
+import createAuditLog from "@/lib/create-audit-log";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -43,6 +46,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageLinkHTML,
       },
     });
+
+    await createAuditLog({
+      entityTitle: board.title,
+      entityId: board.id,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.CREATE
+    })
   } catch (error) {
     return {
       error: "Failed to create",
